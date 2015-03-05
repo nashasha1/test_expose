d2c90eef61c707afa5ec41afc6cecbf4caa6fc52
root <root@ip-172-31-22-137.us-west-2.compute.internal>
2015-03-05 08:18:24 +0000

    fix

--- run.py
+++ run.py
@@ -16,13 +16,13 @@ try:
     cur.execute("INSERT INTO test(num, data)VALUES(%s, %s)", (1, 'aaa'))
     cur.execute("INSERT INTO test(num, data)VALUES(%s, %s)", (2, 'bbb'))
     cur.execute("INSERT INTO test(num, data)VALUES(%s, %s)", (3, 'ccc'))
+    conn.commit()
 except Exception, ex:
     print ex
 # cur.execute("CREATE TABLE auto_test(flag integer);")
 # cur.execute("INSERT INTO auto_test(flag)VALUES(%s)", '1')
 cur.execute("SELECT * FROM test;")
 # cur.execute("update auto_test set flag = 0 ;")
-conn.commit()
 rows = cur.fetchall()        # all rows in table
 print(rows)
 for i in rows:
